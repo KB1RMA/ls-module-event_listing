88985479a35ef7e3bc639d0d363cc801fd6edbe3
Chris Snyder <chris@keyedupmedia.com>
2013-01-16 17:03:50 -0500

    Add single event action

--- classes/eventlisting_actions.php
+++ classes/eventlisting_actions.php
@@ -7,5 +7,11 @@ class EventListing_Actions extends Cms_ActionScope {
 		$this->data['events'] = EventListing_Event::create()->find_all();
 		
 	}
+	
+	public function event() {
+
+		$this->data['event'] = EventListing_Event::create()->find_by_url_title( $this->request_param(-1) );
+
+	}
 
 }
